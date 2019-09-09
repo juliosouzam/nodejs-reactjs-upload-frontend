eb7d9d6c6c26573331c4726597f4365d18175498
Júlio César <julio.souzam@gmail.com>
2019-09-08 22:50:14 -0300

    frontend ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { uniqueId } from "lodash";
+import filesize from "filesize";
+import GlobalStyles from "./styles/global";
+import api from "./services/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Content } from "./styles";
+
+import Upload from "./components/Upload";
+import FileList from "./components/FileList";
+
+export default class App extends Component {
+  state = {
+    uploadedFiles: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("/files");
+
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file._id,
+        name: file.name,
+        readebleSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url
+      }))
+    });
+  }
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      readebleSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null
+    }));
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
+
+    uploadedFiles.forEach(this.processUpload);
+  };
+
+  uploadedFile = (id, data) => {
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+        return id === uploadedFile.id
+          ? { ...uploadedFile, ...data }
+          : uploadedFile;
+      })
+    });
+  };
+
+  processUpload = uploadedFile => {
+    const data = new FormData();
+
+    data.append("file", uploadedFile.file, uploadedFile.name);
+
+    api
+      .post("/files", data, {
+        onUploadProgress: e => {
+          const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+          this.uploadedFile(uploadedFile.id, {
+            progress
+          });
+        }
+      })
+      .then(response => {
+        this.uploadedFile(uploadedFile.id, {
+          uploaded: true,
+          id: response.data._id,
+          url: response.data.url
+        });
+      })
+      .catch(() => {
+        this.uploadedFile(uploadedFile.id, {
+          error: true
+        });
+      });
+  };
+
+  handleDelete = async id => {
+    await api.delete(`/files/${id}`);
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+    });
+  };
+
+  componentWillUnmount() {
+    this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview));
+  }
+
+  render() {
+    const { uploadedFiles } = this.state;
+    return (
+      <Container>
+        <Content>
+          <Upload onUpload={this.handleUpload} />
+          {!!uploadedFiles.length && (
+            <FileList files={uploadedFiles} onDelete={this.handleDelete} />
+          )}
+        </Content>
+        <GlobalStyles />
+      </Container>
+    );
+  }
 }
-
-export default App;
---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 400px;
+  margin: 30px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+`;
